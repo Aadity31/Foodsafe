d243b41b97c74490e557843bcef454a462efecad
Aadity31 <adityadimri04@gmail.com>
2026-02-17 11:40:30 +0530

    update sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'https://foodsave.example.com',
+  siteUrl: process.env.SITE_URL || 'https://foodsafe.apsgroupco.com/',
   generateRobotsTxt: true,
   outDir: './public',
   generateIndexSitemap: false,
